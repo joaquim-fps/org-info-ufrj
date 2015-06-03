ccd79499d22779bb40c0f38df8b58b7c1016b08a
Joaquim <joaquim.fps95@gmail.com>
2015-06-03 17:24:35 -0300

    -Removendo arquivos antigos

--- js/calcTarifa.js
+++
@@ -1,4 +0,0 @@
-function calcTarifa(distance) {
-	document.getElementById('displayDistancia').innerHTML = distance;
-	document.getElementById('displayTarifa').innerHTML = "R$" + (4.5 + 0.25 * parseInt(distance.substr(0, distance.indexOf(" "))) * 20);
-}
