cb3b981918ab2d8ddb3f7703e4c750c641270827
Joaquim <joaquim.fps95@gmail.com>
2015-06-07 18:57:11 -0300

    *Adicionando informação da bandeira 2

--- app/views/templates/sidebar/observations.blade.php
+++ app/views/templates/sidebar/observations.blade.php
@@ -2,6 +2,7 @@
   <h4>Observações:</h4>
 
   <p>Bandeirada: <span class="bandeirada">R$5,20</span></p>
-  <p>Quilômetro Rodado: <span class="km-rodado">R$2,05</span></p>
+  <p>Quilômetro Rodado (Bandeira 1): <span class="km-rodado">R$2,05</span></p>
+  <p>Quilômetro Rodado (Bandeira 2): <span class="km-rodado">R$2,46</span></p>
   <p>Hora Parada: <span class="hora-parada">R$25,83</span></p>
 </div>
